1b50c1b2ea289ce1bf25eb9ee66be11fddbcda5a
Everton Sacramento <nazare.everton@gmail.com>
2022-09-30 13:08:30 -0300

    Primeira parte da função  de adicionar tarefa, criação do estilos dos cards de tarefas e refatoração do CSS e do javascript

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 const btn_days = document.querySelectorAll(".btn-days")
 
 //Referência dos botoes de adicionar e remover
-const btn_addWork = document.querySelector(".work__btn--ad")
+const btn_addWork = document.querySelector(".work__btn--add")
 const btn_removeWork = document.querySelector(".work__btn--remove")
 
 //Referência dos botoes de adicionar e remover localStorage
@@ -14,6 +14,12 @@ const getElement = (atr) =>{
     return document.querySelector(atr)
 }
 
+
+function factoryWork(time, desc){
+    this.time = time
+    this.desc = desc
+}
+
 //Função imediatamente invocada para manter a referÊncia dos dados salvos 
 const handleSchedule = (() =>{
 
@@ -21,63 +27,75 @@ const handleSchedule = (() =>{
         Chave: id do dia da semana (1-seg, 2-ter, 3-qua, 4-qui, 5-sex, 6-sab, 7-dom)
         Valor: Objeto --Chave: hora das atividades convertida em minutos
                         Valor: array que contem a descricao das atividades
-
     */
+   let schedule = {
 
-    let schedule = {
-       1 : {
-            5 : ["Comer", "Dormir"],
-            6 : ["trabalhar"],
-            1:  ["Viajar"]
-        },
+        mon : {},
+        tue : {},
+        wed : {},
+        thu : {},
+        fri : {},
+        sat : {},
+        sun : {},
 
-       2 : {},
-       3 : {},
-       4 : {},
-       5 : {},
-       6 : {},
-       7 : {},
+        lastId : 1
     }
 
     return{
 
-        getScheduleToDay : (idDay) =>{
+        getScheduleByDay : (idDay) =>{
+            console.log(schedule)
             return schedule[idDay]
         },
     
         setWork : (work, idDay) =>{
-            let day = getScheduleToDay(idDay)
+            let day = schedule[idDay]
     
             if(day.hasOwnProperty(work.time)){
-                day[work.time].push(work.desc)
+                day[work.time].push({id: schedule["lastId"], desc:work.desc})
+                schedule["lastId"] = schedule["lastId"] + 1
             }
             else{
-                day[work.time] = [work.desc]
+        
+                day[work.time] = [{id: schedule["lastId"], desc:work.desc}]
+                schedule["lastId"] = schedule["lastId"] + 1
             }
         }
-
     }
  
 })()
 
-//Função para renderizar os dados das atividades salvas no DOM
-const renderSchedule = (key, value) =>{
+
+//Função para limpar os dados na DOM
+const clearScheduleOnDOM = () =>{
+    let containerWork = getElement(".container-works__content")
+    let containerHour = getElement(".panel-works__hour")
+
+    containerHour.innerHTML = ""
+    containerWork.innerHTML = ""
+}
+
+//Função para renderizar os dados das atividades no DOM
+const renderSchedule = (key, value, idDay) =>{
     let containerWork = getElement(".container-works__content")
     let containerHour = getElement(".panel-works__hour")
 
     let cardHour = document.createElement("div")
-    //cardHour.classList.add("ESTILIZAR A CLASSE DO ELEMENTO")
+    cardHour.classList.add(idDay)
     cardHour.innerHTML = key
     containerHour.appendChild(cardHour)
 
     let containerCardWork =  document.createElement("div")
     containerCardWork.classList.add("content-works__container-card")
 
-    value.forEach(desc => {
+    value.forEach(({desc, id}) => {
 
         let cardDesc = document.createElement("div")
-        //cardDesc.classList.add("ESTILIZAR A CLASSE DO ELEMENTO")
+        cardDesc.classList.add(idDay)
+        cardDesc.classList.add("card-work")
         cardDesc.innerHTML = desc
+        cardDesc.setAttribute("data-id", id)
+        cardDesc.setAttribute("data-time", key)
         containerCardWork.appendChild(cardDesc)
     })
 
@@ -89,24 +107,31 @@ const renderSchedule = (key, value) =>{
 const getDay = (e) => {
     const idDay = e.target.getAttribute("data-id")
 
-    console.log(idDay)
-    
-    let schedule = handleSchedule.getScheduleToDay(idDay)
+    let schedule = handleSchedule.getScheduleByDay(idDay)
     let keys = Object.keys(schedule)
     keys.sort()
 
+    clearScheduleOnDOM()
+
     keys.forEach(key => {
-        renderSchedule(key, schedule[key])
-    })
-    
+        renderSchedule(key, schedule[key], idDay)
+    })   
 }
 
-const setWorkToDay = ()=>{
-    const timeWork = getElement(".work__select--day").value
+const createWork = ()=>{
+    const timeWork = getElement(".work__input--hour").value
     const descWork = getElement(".work__input--desc").value
- 
 
+    const select = getElement(".work__select--day")
+    const idDay = select.options[select.selectedIndex].value;
+
+
+    const work = new factoryWork(timeWork, descWork)
+
+    handleSchedule.setWork(work, idDay)
 }
+
+
 //Adicinando listener de evento dos botos de selecionar o dia
 const addListenerBtnDays = () => {
     btn_days.forEach((btn)=>{
@@ -115,6 +140,9 @@ const addListenerBtnDays = () => {
         })
     })
 }
+
+btn_addWork.addEventListener("click", createWork)
+
 
 const init = () =>{
     addListenerBtnDays()
