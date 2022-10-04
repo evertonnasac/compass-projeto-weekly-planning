775e2b4dedcf262f8d6dd7f9774865ca5a500d4c
Everton Sacramento <nazare.everton@gmail.com>
2022-10-03 23:54:49 -0300

    implementando função que pega a hora e data atual, refatoração do javscript e melhorias no css

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,7 @@ const getElement = (atr) =>{
     return document.querySelector(atr)
 }
 
-
+//Função para criar objetos works
 function factoryWork(time, desc){
     this.time = time
     this.desc = desc
@@ -44,14 +44,17 @@ const handleSchedule = (() =>{
 
     return{
 
+        //Retornando objeto correspondente ao dia recebido no parametro
         getScheduleByDay : (idDay) =>{
-            console.log(schedule)
-            return schedule[idDay]
+            
+            let scheduleDay = {...schedule[idDay]}
+            return scheduleDay
         },
-    
+        
+        //Salvar a tarefa no objeto schedule - Recebe a tarefa e o dia para qual sera armazenada
         setWork : (work, idDay) =>{
             let day = schedule[idDay]
-    
+            
             if(day.hasOwnProperty(work.time)){
                 day[work.time].push({id: schedule["lastId"], desc:work.desc})
                 schedule["lastId"] = schedule["lastId"] + 1
@@ -63,6 +66,7 @@ const handleSchedule = (() =>{
             }
         },
 
+        //Remover uma tarefa selecionada - Recebe o id da tarefa, o dia e a hora
         removeWorkByDay : (idParam, idDay, timeParam) => {
             let time = schedule[idDay][timeParam]
             schedule[idDay][timeParam] = time.filter(({id}) => id != idParam)
@@ -73,11 +77,13 @@ const handleSchedule = (() =>{
             
         },
 
+        //Remove todas as tarefas do dia selecionado - Recebe o dia selecionado
         removeAllWorksByDay : (dayCurrent) =>{
             schedule[dayCurrent] = {}
 
         },
 
+        //Salva o estado atual da agenda no localstorage
         saveLocalStorage : () =>{
             localStorage.setItem("schedule", JSON.stringify(schedule))
             localStorage.getItem("schedule") ? 
@@ -86,11 +92,31 @@ const handleSchedule = (() =>{
             
         },
 
+        //Deleta a ultima agenda salva no localstorage
         deleteLocalStorage : () =>{
             localStorage.removeItem("schedule")
-            alert("Os dados foram apagados, se desejar manter o trabalho atual, salve no localstorage")
+
+            if(!localStorage.getItem("schedule")){
+                alert("Os dados foram excuídos com sucesso")
+
+                //Limpar os dados que ja foram renderizados no DOM
+                clearScheduleOnDOM()
+
+                //Remover todos vestigios que ainda tiverem salvos na memoria
+                for (const key in schedule) {
+                    schedule[key] = {}
+                }
+
+                schedule["lastId"] = 1
+
+            }
+            else{
+                alert("Não foi possível excluir os dados salvos no LocalStorage")
+            } 
+            
         },
 
+        //Pega a ultima agenda salva no localstorage
         getLocalStorage : () => {
             schedule = JSON.parse(localStorage.getItem("schedule")) || schedule
 
@@ -127,53 +153,72 @@ const clearScheduleOnDOM = () =>{
 }
 
 
-//Função para renderizar os as atividades no DOM
-const renderSchedule = (key, value, idDay) =>{
-    let containerWork = getElement(".container-works__content")
-    let containerHour = getElement(".panel-works__hour")
+//Função para renderizar  as atividades da agenda no DOM - Recebe o dia selecionado
+const renderSchedule = (idDay) =>{
 
-    let cardHour = document.createElement("div")
-    cardHour.classList.add(idDay)
-    cardHour.innerHTML = key.replace(":", "h") + "m"
-    containerHour.appendChild(cardHour)
+    //objeto correspondente ao dia passado no parametro da função
+    let scheduleDay = handleSchedule.getScheduleByDay(idDay)
 
-    let containerCardWork =  document.createElement("div")
-    containerCardWork.classList.add("content-works__container-card")
+    //garantir a renderização pela ordem da hora da atividade
+    let hours = Object.keys(scheduleDay)
+    hours.sort()
+    
+    hours.forEach(hour => {
+        
+        //Pegando o array (horario) com as atividades salvas 
+        let scheduleWorks = scheduleDay[hour]
 
-    value.forEach(({desc, id}) => {
+        let containerWork = getElement(".container-works__content")
+        let containerHour = getElement(".panel-works__hour")
+    
+        let cardHour = document.createElement("div")
+        cardHour.classList.add(idDay)
+        cardHour.innerHTML = hour.replace(":", "h") + "m"
+        containerHour.appendChild(cardHour)
+    
+        let containerCardWork =  document.createElement("div")
+        containerCardWork.classList.add("content-works__container-card")
+    
+        scheduleWorks.forEach(({desc, id}) => {
+    
+            //Criando o card com a descrição da atividade
+            let cardDesc = document.createElement("div")
+            cardDesc.classList.add(idDay)
+            cardDesc.classList.add("card-work")
+            cardDesc.innerHTML = desc
+            cardDesc.setAttribute("data-id", id)
+            cardDesc.setAttribute("data-time", hour)
+            containerCardWork.appendChild(cardDesc)
+    
+            //Criando o botao de remover a atividade
+            let btn_remove = document.createElement("button")
+            btn_remove.setAttribute("value", "Apagar")
+            btn_remove.classList.add("btn__remove")
+            btn_remove.innerHTML = "Apagar"
+            btn_remove.setAttribute("data-id", id)
+            btn_remove.setAttribute("data-time", hour)
+            btn_remove.setAttribute("data-day", idDay)
+    
+            btn_remove.addEventListener("click", (e) =>{
+                actionUser("Deseja apagar essa atividade?", removeWork, e)})
+    
+            cardDesc.appendChild(btn_remove)
+        })
+    
+        containerWork.appendChild(containerCardWork)
 
-        //Criando o card com a descrição da atividade
-        let cardDesc = document.createElement("div")
-        cardDesc.classList.add(idDay)
-        cardDesc.classList.add("card-work")
-        cardDesc.innerHTML = desc
-        cardDesc.setAttribute("data-id", id)
-        cardDesc.setAttribute("data-time", key)
-        containerCardWork.appendChild(cardDesc)
+        //Verificando se existe mais de uma atividade no Array (horario), para sinalizar o conflito  a view
+        if(scheduleWorks.length > 1){
+            let img = document.createElement("img")
+            img.setAttribute("src", "./assets/images/redline.png")
+            containerCardWork.appendChild(img)
+            containerCardWork.classList.add("conflict")
+            cardHour.classList.add("conflict")
+        }
 
-        //Criando o botao de remover a atividade
-        let btn_remove = document.createElement("button")
-        btn_remove.setAttribute("value", "Apagar")
-        btn_remove.classList.add("btn__remove")
-        btn_remove.innerHTML = "Apagar"
-        btn_remove.setAttribute("data-id", id)
-        btn_remove.setAttribute("data-time", key)
-        btn_remove.setAttribute("data-day", idDay)
-
-        btn_remove.addEventListener("click", (e) =>{
-            actionUser("Deseja apagar essa atividade?", removeWork, e)})
-
-        cardDesc.appendChild(btn_remove)
     })
 
-    containerWork.appendChild(containerCardWork)
-    if(value.length > 1){
-        let img = document.createElement("img")
-        img.setAttribute("src", "./assets/images/redline.png")
-        containerCardWork.appendChild(img)
-        containerCardWork.classList.add("conflict")
-        cardHour.classList.add("conflict")
-    }
+   
     
 }
 
@@ -181,19 +226,14 @@ const renderSchedule = (key, value, idDay) =>{
 const getDay = (day) => {
     const idDay = day
 
-    handleCurrentDay.setCurrentDay(idDay)
-
+    handleCurrentDay.setCurrentDay(day)
+    
     setBtnActiveDay()
-
-    let schedule = handleSchedule.getScheduleByDay(idDay)
-    let keys = Object.keys(schedule)
-    keys.sort()
 
     clearScheduleOnDOM()
 
-    keys.forEach(key => {
-        renderSchedule(key, schedule[key], idDay)
-    })   
+    renderSchedule(idDay)
+    
 }
 
 //Função para destacar o botao correspondente ao dia atual
@@ -212,17 +252,17 @@ const setBtnActiveDay = () =>{
     })
 }
 
-//Função para criar um objeto "work" e passa para a função setWork que salva os dados
+//Função para criar um objeto "work" e passar para a função setWork para salvar na agenda
 const createWork = ()=>{
-    const timeWork = getElement(".work__input--hour").value
-    const descWork = getElement(".work__input--desc").value
+    const timeWork = getElement(".work__input--hour")
+    const descWork = getElement(".work__input--desc")
 
     const select = getElement(".work__select--day")
     const idDay = select.options[select.selectedIndex].value;
 
-    const work = new factoryWork(timeWork, descWork)
+    const work = new factoryWork(timeWork.value, descWork.value)
 
-    if(timeWork && descWork){
+    if(timeWork.value && descWork.value){
         handleSchedule.setWork(work, idDay)
 
         timeWork.value = ""
@@ -231,11 +271,11 @@ const createWork = ()=>{
         getDay(idDay)
     }
     else{
-        if(!descWork){
+        if(!descWork.value){
             alert("Por favor, preencha a descrição da atividade corretamente")
             return
         }
-        if(!timeWork){
+        if(!timeWork.value){
             alert("Por favor, preencha o horário da atividade corretamente")
         }
     }
@@ -261,6 +301,39 @@ const deleteAllWorksByDay = () =>{
 
 }
 
+//Pegar a hora em tempo real
+const getTimeNow = () =>{
+
+    const pTime = getElement(".container-time--hour")
+    const pDate = getElement(".container-time--date")
+    
+    const optionDate = {
+        year: 'numeric',
+        month: 'long',
+        day: "numeric",
+    }
+
+    const optionTime = {
+        hour : "numeric",
+        minute : "numeric"
+    }
+
+    let date = new Date().toLocaleDateString('pt-br', optionDate)
+    let time = new Date().toLocaleTimeString("pt-br", optionTime)
+
+    pTime.innerHTML = time
+    pDate.innerHTML = date
+  
+}
+
+//Garantir a confirmação do usuario antes de executar uma ação crítica
+const actionUser = (message, callback, event) => {
+    let opt = confirm(message)
+    if(opt){
+        callback(event)
+    }
+}
+
 
 ////Listener de eventos
 
@@ -272,12 +345,6 @@ const addListenerBtnDays = () => {
     })
 }
 
-const actionUser = (message, callback, event) => {
-    let opt = confirm(message)
-    if(opt){
-        callback(event)
-    }
-}
 
 btn_addWork.addEventListener("click", createWork)
 
@@ -293,27 +360,18 @@ btn_removeStorage.addEventListener("click", () =>{
 })
 
 const init = () =>{
+
     addListenerBtnDays()
+
     handleSchedule.getLocalStorage()
+
+    setInterval(() => {
+        getTimeNow()
+    }, 1000);
+
+    const day = new Date().toLocaleDateString("en-us", {weekday: 'short'}).toLocaleLowerCase()
+    getDay(day)
+
 }
-
-/*
-let div2 = document.querySelector(".panel-works__hour")
-let div = document.querySelector(".main__panel--works")
-
-div.addEventListener("scroll", (e)=>{
-    let p = e.target.scrollLeft
-    if(p > 0){
-        console.log(p)
-        div2.style.position  = "fixed"
-    }
-    if(p==0){
-        div2.style.position = "static"
-    }
-    
-})*/
-
-
-
 
 init()
